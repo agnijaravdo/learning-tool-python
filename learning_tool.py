c5b60b8832ce2fee0a5f27d7134a87e5df0f83cf
Agnija Ravdo <41111594+agnijaravdo@users.noreply.github.com>
2024-05-19 20:50:19 +0300

    add validation for selecting practice or test modes

--- learning_tool.py
+++ learning_tool.py
@@ -68,12 +68,31 @@ def show_starting_menu():
         options = [item.value for item in StartMenuItem]
         terminal_menu = TerminalMenu(
             options,
-            title="\nUse ↓ or ↑ arrow keys to navigate and 'Enter' to select:\n",
+            title="\nℹ️ Use ↓ or ↑ arrow keys to navigate and 'Enter' to select:\n",
             menu_cursor_style=("fg_green", "bold"),
         )
         menu_entry_index = terminal_menu.show()
-        if StartMenuItem(options[menu_entry_index]) == StartMenuItem.ADD_QUESTIONS:
+        selected_mode = StartMenuItem(options[menu_entry_index])
+
+        clear_screen()
+        if selected_mode == StartMenuItem.ADD_QUESTIONS:
             show_select_question_type_menu()
+        elif selected_mode == StartMenuItem.PRACTICE_MODE:
+            questions_count = get_number_of_questions()
+            if questions_count < 5:
+                print(
+                    f"\n❗ To select '{StartMenuItem.PRACTICE_MODE.value}' you need to have minimum of 5 questions. Current count of questions is {questions_count}"
+                )
+            else:
+                print("opening practice mode")
+        elif selected_mode == StartMenuItem.TEST_MODE:
+            questions_count = get_number_of_questions()
+            if questions_count < 5:
+                print(
+                    f"\n❗ To select '{StartMenuItem.TEST_MODE.value}' you need to have minimum of 5 questions. Current count of questions is {questions_count}"
+                )
+            else:
+                print("opening test mode")
 
 
 def show_select_question_type_menu():
@@ -81,7 +100,7 @@ def show_select_question_type_menu():
         options = [question.value for question in QuestionType]
         terminal_menu = TerminalMenu(
             options,
-            title="\nUse ↓ or ↑ arrow keys to navigate and 'Enter' to select the question type you want to add:\n",
+            title="\nℹ️ Use ↓ or ↑ arrow keys to navigate and 'Enter' to select the question type you want to add:\n",
             menu_cursor_style=("fg_green", "bold"),
         )
         menu_entry_index = terminal_menu.show()
@@ -91,6 +110,10 @@ def show_select_question_type_menu():
             return
 
         add_question(selected_type)
+
+
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear")
 
 
 def add_question(question_type):
@@ -153,6 +176,16 @@ def write_questions_to_file(questions):
                     "answers": question.answers,
                 }
             )
+
+
+def get_number_of_questions():
+    with open("data/questions.csv", "r") as file:
+        reader = csv.reader(file)
+        next(reader)
+        questions_count = 0
+        for _ in reader:
+            questions_count += 1
+    return questions_count
 
 
 def main():
